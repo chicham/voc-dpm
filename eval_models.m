321a472201b0b8e93b89f14a9c9b1e72eb0b00ef
Hicham <hicham@randrianarivo.net>
2014-10-27 15:55:53 +0100

    specific config

--- eval_models.m
+++ eval_models.m
@@ -1,9 +1,9 @@
 function eval_models(path)
 
 startup;
-global VOC_CONFIG_OVERRIDE
-VOC_CONFIG_OVERRIDE=@ilsvrc_voc_config_override
-load('categories.mat')
+global VOC_CONFIG_OVERRIDE;
+VOC_CONFIG_OVERRIDE=@ilsvrc_voc_config_override;
+load('categories.mat');
 
 for i=1:80
   cls = strtrim(categories(i, :));
