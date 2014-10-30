daaf6b6b5d1288b252d30f4987ec04e2fd8fdef5
Hicham <hicham@randrianarivo.net>
2014-10-30 20:59:26 +0100

    up

--- eval_models.m
+++ eval_models.m
@@ -4,13 +4,16 @@ startup;
 global VOC_CONFIG_OVERRIDE;
 VOC_CONFIG_OVERRIDE=@ilsvrc_voc_config_override;
 load('categories.mat');
+conf = voc_conf()
 
 for i=1:80
   cls = strtrim(categories(i, :));
   fname = sprintf('%s/%s_final.mat', path, cls)
 
   if exist(fname) > 0
-    eval_model(cls, model)
+    if exist(sprintf('%s/%s/%s/%s_pr_val_2014.mat', conf.paths.base_dir, conf.project, conf.pascal.year, cls)) == 0
+      eval_model(cls, model)
+    end
   end
 
 end
