84a0e4e151c4e99eb3e185b1c500eb7bfb2adda3
Hicham <hicham@randrianarivo.net>
2014-11-05 18:48:25 +0100

    up

--- coco2pascal.py
+++ coco2pascal.py
@@ -117,7 +117,7 @@ def category_set(category, dbpath, year='2014'):
     def f(imageset, annotations, year, dst, template, subset, category):
         imageset = imageset.format(year=year, subset=subset)
         dst = dst.format(year=year, category=category, subset=subset)
-        for name in imageset:
+        for name in imageset.lines(retain=False):
             anno = objectify.fromstring(path(annotations.format(year=year, filename=name)).text())
             if category in set(pluck('name', anno['object'])):
                 present = 1
